bfd99849d9fa0f45ae7048f0b36a58d1dedb2abe
Frank <franksu49@gmail.com>
2019-10-04 12:43:25 -0400

    added movelog class to table

--- src/com/chess/gui/Table.java
+++ src/com/chess/gui/Table.java
@@ -184,6 +184,40 @@ public class Table {
         abstract BoardDirection opposite();
     }
 
+    // class to store list of current moves in game
+    public static class MoveLog {
+
+        private final List<Move> moves;
+        MoveLog() {
+            this.moves = new ArrayList<>();
+        }
+
+        public List<Move> getMoves() {
+            return this.moves;
+        }
+
+        public void addMove(final Move move) {
+            this.moves.add(move);
+        }
+
+        public int size() {
+            return this.moves.size();
+        }
+
+        public void clear() {
+            this.moves.clear();
+        }
+
+        public Move removeMove(int index) {
+            return this.moves.remove(index);
+        }
+
+        public boolean removeMove(final Move move) {
+            return this.moves.remove(move);
+        }
+
+    }
+
     //inner class to represent tiles on board
     private class TilePanel extends JPanel {
 
