da1c3dc9b9c7279b706b277c25cdd9011f180234
Frank <franksu49@gmail.com>
2019-10-04 12:59:22 -0400

    added takenpiecespanel

---
+++ src/com/chess/gui/TakenPiecesPanel.java
@@ -0,0 +1,114 @@
+package com.chess.gui;
+
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.Piece;
+import com.google.common.primitives.Ints;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.border.EtchedBorder;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ArrayList;
+
+import static com.chess.gui.Table.*;
+
+//gui panel to represent taken pieces in the game
+public class TakenPiecesPanel extends JPanel {
+    //north and south panel, one for each player's pieces
+    private final JPanel northPanel;
+    private final JPanel southPanel;
+
+    private static final Color PANEL_COLOR = Color.LIGHT_GRAY;
+    private static final Dimension TAKEN_PIECES_DIMENSION = new Dimension(40, 80);
+    private static final EtchedBorder PANEL_BORDER = new EtchedBorder(EtchedBorder.RAISED);
+
+    //panel is divided into 2 vertically
+    public TakenPiecesPanel() {
+        super (new BorderLayout());
+        setBackground(Color.cyan);
+        setBorder(PANEL_BORDER);
+        this.northPanel = new JPanel(new GridLayout(8, 2));
+        this.southPanel = new JPanel(new GridLayout(8, 2));
+        this.northPanel.setBackground(PANEL_COLOR);
+        this.southPanel.setBackground(PANEL_COLOR);
+        this.add(this.northPanel, BorderLayout.NORTH);
+        this.add(this.southPanel, BorderLayout.SOUTH);
+        setPreferredSize(TAKEN_PIECES_DIMENSION);
+    }
+
+    public void redo(final MoveLog moveLog) {
+        southPanel.removeAll();
+        northPanel.removeAll();
+
+        final List<Piece> whiteTakenPieces = new ArrayList<>();
+        final List<Piece> blackTakenPieces = new ArrayList<>();
+
+        //iterate through moves in move log
+        for (final Move move : moveLog.getMoves()) {
+            //if move is attack move, retrieve piece taken and add to
+            //taken pieces list
+            if(move.isAttack()) {
+                final Piece takenPiece = move.getAttackedPiece();
+                if (takenPiece.getPieceAlliance().isWhite()) {
+                    whiteTakenPieces.add(takenPiece);
+                }
+                else if (takenPiece.getPieceAlliance().isBlack()) {
+                    blackTakenPieces.add(takenPiece);
+                }
+                else {
+                    throw new RuntimeException("Should not reach here!");
+                }
+            }
+        }
+
+        //sort pieces on panel based on value
+        Collections.sort(whiteTakenPieces, new Comparator<Piece>() {
+            @Override
+            public int compare(Piece o1, Piece o2) {
+                return Ints.compare(o1.getPieceValue(), o2.getPieceValue());
+            }
+        });
+        Collections.sort(blackTakenPieces, new Comparator<Piece>() {
+            @Override
+            public int compare(Piece o1, Piece o2) {
+                return Ints.compare(o1.getPieceValue(), o2.getPieceValue());
+            }
+        });
+
+        //draw piece on panel
+        for (final Piece takenPiece : whiteTakenPieces) {
+            try {
+                final BufferedImage image = ImageIO.read(new File("art/pieces/plain" +
+                                                        takenPiece.getPieceAlliance().toString().substring(0, 1) +
+                                                        "" + takenPiece.toString()));
+                final ImageIcon icon = new ImageIcon(image);
+                final JLabel imageLabel = new JLabel();
+                this.southPanel.add(imageLabel);
+            } catch(final IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for (final Piece takenPiece : blackTakenPieces) {
+            try {
+                final BufferedImage image = ImageIO.read(new File("art/pieces/plain" +
+                        takenPiece.getPieceAlliance().toString().substring(0, 1) +
+                        "" + takenPiece.toString()));
+                final ImageIcon icon = new ImageIcon(image);
+                final JLabel imageLabel = new JLabel();
+                this.southPanel.add(imageLabel);
+            } catch(final IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        validate();
+    }
+}
