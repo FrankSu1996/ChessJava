1b0f97fb8b2212a92994ec2254a5942d5bc3c9cb
Frank <franksu49@gmail.com>
2019-10-08 15:27:17 -0400

    fixed bug in takePiecesPanel, should now work

--- src/com/chess/gui/TakenPiecesPanel.java
+++ src/com/chess/gui/TakenPiecesPanel.java
@@ -25,14 +25,14 @@ public class TakenPiecesPanel extends JPanel {
     private final JPanel northPanel;
     private final JPanel southPanel;
 
-    private static final Color PANEL_COLOR = Color.LIGHT_GRAY;
+    private static final Color PANEL_COLOR = Color.CYAN;
     private static final Dimension TAKEN_PIECES_DIMENSION = new Dimension(40, 80);
     private static final EtchedBorder PANEL_BORDER = new EtchedBorder(EtchedBorder.RAISED);
 
     //panel is divided into 2 vertically
     public TakenPiecesPanel() {
         super (new BorderLayout());
-        setBackground(Color.cyan);
+        setBackground(PANEL_COLOR);
         setBorder(PANEL_BORDER);
         this.northPanel = new JPanel(new GridLayout(8, 2));
         this.southPanel = new JPanel(new GridLayout(8, 2));
@@ -85,11 +85,11 @@ public class TakenPiecesPanel extends JPanel {
         //draw piece on panel
         for (final Piece takenPiece : whiteTakenPieces) {
             try {
-                final BufferedImage image = ImageIO.read(new File("art/pieces/plain" +
-                                                        takenPiece.getPieceAlliance().toString().substring(0, 1) +
-                                                        "" + takenPiece.toString()));
+                final BufferedImage image = ImageIO.read(new File("art/pieces/plain/" +
+                        takenPiece.getPieceAlliance().toString().substring(0, 1) + "" + takenPiece.toString()
+                        + ".gif"));
                 final ImageIcon icon = new ImageIcon(image);
-                final JLabel imageLabel = new JLabel();
+                final JLabel imageLabel = new JLabel(new ImageIcon(icon.getImage().getScaledInstance(icon.getIconWidth() - 15, icon.getIconHeight() - 15, image.SCALE_SMOOTH)));
                 this.southPanel.add(imageLabel);
             } catch(final IOException e) {
                 e.printStackTrace();
@@ -98,12 +98,12 @@ public class TakenPiecesPanel extends JPanel {
 
         for (final Piece takenPiece : blackTakenPieces) {
             try {
-                final BufferedImage image = ImageIO.read(new File("art/pieces/plain" +
-                        takenPiece.getPieceAlliance().toString().substring(0, 1) +
-                        "" + takenPiece.toString()));
+                final BufferedImage image = ImageIO.read(new File("art/pieces/plain/" +
+                        takenPiece.getPieceAlliance().toString().substring(0, 1) + "" + takenPiece.toString()
+                        + ".gif"));
                 final ImageIcon icon = new ImageIcon(image);
-                final JLabel imageLabel = new JLabel();
-                this.southPanel.add(imageLabel);
+                final JLabel imageLabel = new JLabel(new ImageIcon(icon.getImage().getScaledInstance(icon.getIconWidth() - 15, icon.getIconHeight() - 15, image.SCALE_SMOOTH)));
+                this.northPanel.add(imageLabel);
             } catch(final IOException e) {
                 e.printStackTrace();
             }
